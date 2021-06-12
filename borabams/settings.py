f9ad6aef733d8d46af4b78000bab13286cdd7151
jirighoemaanuel <emmaweyinmi@gmail.com>
2021-06-12 01:32:51 +0100

    commit

--- borabams/settings.py
+++ borabams/settings.py
@@ -148,7 +148,7 @@ AUTHENTICATION_BACKENDS = ['social_core.backends.facebook.FacebookOAuth2', 'soci
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
